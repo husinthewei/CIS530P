473a125c8c72eea83a736b09dfa89a8c499f0823
sfleischer <fls@seas.upenn.edu>
2019-04-19 12:27:22 -0400

    Added verbose option for precision/recall

--- score.py
+++ score.py
@@ -1,12 +1,13 @@
 import json
 import argparse
 import warnings 
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--goldfile', type=str, required=True)
 parser.add_argument('--predfile', type=str, required=True)
+parser.add_argument('--verbose', '-v', action='store_true')
 
 
 def read_labels(file):
@@ -68,6 +69,15 @@ def compute_micro_f1(gold, pred, field):
     y_pred = [x[field] for x in pred]
     return f1_score(y_true, y_pred, average='micro')
 
+def compute_pr(gold, pred, field):
+    y_true = [x[field] for x in gold]
+    y_pred = [x[field] for x in pred]
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        precision = precision_score(y_true, y_pred, average='macro')
+        recall = recall_score(y_true, y_pred, average='macro')
+        return precision, recall
+
 
 def main(args):
     gold = read_labels(args.goldfile)
@@ -94,6 +104,26 @@ def main(args):
     print("Date macro f1        : " + str(date_f1))
     print("Num killed macro f1  : " + str(n_killed_f1))
     print("Num injured macro f1 : " + str(n_injured_f1))
+    print()
+
+    if not args.verbose:
+        return 
+
+    addr_p, addr_r = compute_pr(gold, pred, "address")
+    date_p, date_r = compute_pr(gold, pred, "shooting_date")
+    n_killed_p, n_killed_r = compute_pr(gold, pred, "n_killed")
+    n_injured_p, n_injured_r = compute_pr(gold, pred, "n_injured")
+
+    print("Address precision     : " + str(addr_p))
+    print("Date precision        : " + str(date_p))
+    print("Num killed precision  : " + str(n_killed_p))
+    print("Num injured precision : " + str(n_injured_p))
+    print()
+
+    print("Address recall       : " + str(addr_r))
+    print("Date recall          : " + str(date_r))
+    print("Num killed recall    : " + str(n_killed_r))
+    print("Num injured recall   : " + str(n_injured_r))
     print()
 
 
