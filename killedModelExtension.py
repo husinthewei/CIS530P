f7b08f8f2f3bf1be0f3e7a4a8279af970ee2f602
sfleischer <fls@seas.upenn.edu>
2019-04-30 16:52:20 -0400

    SVC with new features is doing better than 0 baseline (85% acc)

--- killedModelExtension.py
+++ killedModelExtension.py
@@ -2,13 +2,14 @@
 import sklearn_crfsuite
 import spacy
 import sys
-from sklearn.linear_model import Perceptron
+#from sklearn.linear_model import Perceptron
+from sklearn.svm import SVC
 from sklearn.feature_extraction import DictVectorizer
 
 class KilledModelExtension(object):
     def __init__(self, killed=True):
         self.crf = sklearn_crfsuite.CRF( algorithm='lbfgs', c1=0.1, c2=0.1,max_iterations=100, all_possible_transitions=True)
-        self.model = Perceptron()
+        self.model = SVC()#Perceptron()
         self.nlp = spacy.load("en_core_web_sm")
         injured_labels = ["O-INJ", "1-INJ", "2-INJ", "3-INJ", "4-INJ"]
         killed_labels = ["0-KILL", "1-KILL", "2-KILL", "3-KILL", "4-KILL"]
@@ -49,10 +50,19 @@ class KilledModelExtension(object):
         idx = len(arr) - 1 if num > len(arr) - 1 else num
         return self.arr_labels[idx]
 
+    def __cardinal2number(self, card):
+        num = card.lower()
+        d = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10}
+        if num in d:
+            return d[num]
+        else:
+            return -1
+
 
     def __doc2features(self, doc):
-        death_terms = ["kill", "death", "die", "shot", "fatal"]
-        injured_terms = ["hurt", "shot", "injured", "hospital", "wound"]
+        death_terms = ["kill", "death", "die", "fatal", "killed"]
+        injured_terms = ["hurt", "shot", "injured", "hospital", "wound", "wounded"]
+        roads = ["Rd", "St", "Ave", "Blvd", "Ln", "Dr", "Ter", "Pl", "Ct"]
 
         int_num = 0
         str_num = ""
@@ -71,7 +81,10 @@ class KilledModelExtension(object):
             if ent.label_ == "PERSON":
                 p = ent.text
                 if len(p.split()) > 0:
-                    unique_people.append(p.split()[-1])
+                    lastname = p.split()[-1]
+                    if lastname in roads:
+                        continue
+                    unique_people.append(lastname)
                 else:
                     unique_people.append(p)
 
@@ -79,15 +92,21 @@ class KilledModelExtension(object):
 
 
 
-        features = {
-            # "death_terms": sum([1 if w.lemma_ in death_terms else 0 for w in doc]),
-            # "injured_terms": sum([1 if w.lemma_ in injured_terms else 0 for w in doc]),
-            # "int_number": int(num) if num.isdigit() else 0,
-            # "str_number": num if not num.isdigit() else "",
-            # "int_number": int_num,
-            # "str_number": str_num,
-            "n_people": len(unique_people)
-        }
+        # features = {
+        #     # "death_terms": sum([1 if w.lemma_ in death_terms else 0 for w in doc]),
+        #     # "injured_terms": sum([1 if w.lemma_ in injured_terms else 0 for w in doc]),
+        #     # "int_number": int(num) if num.isdigit() else 0,
+        #     # "str_number": num if not num.isdigit() else "",
+        #     # "int_number": int_num,
+        #     # "str_number": str_num,
+        #     "n_people": len(unique_people),
+        #     "n_killed_terms:" sum([1 if w.text in death_terms else 0 for w in doc]) 
+        # }
+        features = [
+            len(unique_people),
+            sum([1 if w.text in death_terms else 0 for w in doc]),
+            sum([1 if w.text in injured_terms else 0 for w in doc]),
+        ]
 
 
         return features
@@ -113,12 +132,12 @@ class KilledModelExtension(object):
             #         sub_train.append(self.__sent2features(span.string.strip(), "0"))
 
 
-
-            y_train.append(self.assign_label(label[self.field]))
+            #print(sub_train)
+            y_train.append(label[self.field])
             X_train.append(sub_train)
 
 
-        X_train = self.vectorizer.fit_transform(X_train)
+        #X_train = self.vectorizer.fit_transform(X_train)
         self.model.fit(X_train, y_train)
 
     def predict_event(self, event):
@@ -140,12 +159,12 @@ class KilledModelExtension(object):
         X_test = [sub_train]
 
 
-        X_test = self.vectorizer.transform(X_test)
+        #X_test = self.vectorizer.transform(X_test)
 
         y_pred = self.model.predict(X_test)
 
-        n_killed = [self.arr_labels.index(label) for label in y_pred]
-        return max(n_killed)
+        #n_killed = [self.arr_labels.index(label) for label in y_pred]
+        return int(y_pred[0])
 
     # def predict(self, X_events):
     #     return [self.__predict_event(event) for event in X_events]
